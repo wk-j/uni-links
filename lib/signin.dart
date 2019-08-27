bdfc8f30339d52beffdfb0c91aa47fa458e3ce2c
wk-j <somnuk.wk@gmail.com>
2019-08-27 23:45:19 +0700

    GitHub

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,35 +1,74 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:oauth2/oauth2.dart';
 import 'package:uni_links/uni_links.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
-var auth = "https://github.com/login/oauth/authorize";
-var tokenUrl = "https://github.com/login/oauth/access_token";
-var user = "https://api.github.com/user";
-var clientId = "9f18bf43ed3bce16e685";
-var secret = "78331982064908b8a16725e44843e23481fbae46";
-var redirectUrl = "https://localhost/auth";
+final auth = "https://github.com/login/oauth/authorize";
+final tokenUrl = "https://github.com/login/oauth/access_token";
+final user = "https://api.github.com/user";
+const String clientId = "9f18bf43ed3bce16e685";
+final secret = "78331982064908b8a16725e44843e23481fbae46";
+final redirectUrl = "https://localhost/auth";
+
+class GitHubLoginRequest {
+  String clientId;
+  String clientSecret;
+  String code;
+
+  GitHubLoginRequest({this.clientId, this.clientSecret, this.code});
+
+  dynamic toJson() => {
+        "client_id": clientId,
+        "client_secret": clientSecret,
+        "code": code,
+      };
+}
+
+class GitHubLoginResponse {
+  String accessToken;
+  String tokenType;
+  String scope;
+
+  GitHubLoginResponse({this.accessToken, this.tokenType, this.scope});
+
+  factory GitHubLoginResponse.fromJson(Map<String, dynamic> json) =>
+      GitHubLoginResponse(
+        accessToken: json["access_token"],
+        tokenType: json["token_type"],
+        scope: json["scope"],
+      );
+}
+
+/// WIDGET
 
 class Foo extends StatefulWidget {
   @override
   State createState() => new _FooState();
 }
 
+// STATE
 class _FooState extends State<Foo> {
-  AuthorizationCodeGrant _grant;
   StreamSubscription _subs;
 
   @override
   void initState() {
-    _grant = new AuthorizationCodeGrant(
-      clientId,
-      Uri.parse(auth),
-      Uri.parse(tokenUrl),
-      secret: secret,
-    );
-
     _initDeepLinkListener();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _disposeDeepLinkListener();
+    super.dispose();
+  }
+
+  void _disposeDeepLinkListener() {
+    if (_subs != null) {
+      _subs.cancel();
+      _subs = null;
+    }
   }
 
   void _initDeepLinkListener() async {
@@ -38,27 +77,51 @@ class _FooState extends State<Foo> {
     }, cancelOnError: true);
   }
 
-  void _checkDeepLink(String link) {
-    debugPrint("--- link -- $link");
+  Future<GitHubLoginResponse> loginWithGitHub(String code) async {
+    final response = await http.post(
+      "https://github.com/login/oauth/access_token",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: jsonEncode(GitHubLoginRequest(
+        clientId: clientId,
+        clientSecret: secret,
+        code: code,
+      )),
+    );
 
-    if (link.contains("code=")) {
-      var uri = Uri.parse(link);
-      _grant.handleAuthorizationResponse(uri.queryParameters).then((client) {
-        var token = client.credentials.accessToken;
-        debugPrint("--- token -- $token");
+    GitHubLoginResponse loginResponse =
+        GitHubLoginResponse.fromJson(json.decode(response.body));
+
+    return loginResponse;
+  }
+
+  void _checkDeepLink(String link) {
+    if (link != null) {
+      var code = link.substring(link.indexOf(RegExp("code=")) + 20);
+      loginWithGitHub(code).then((github) {
+        print("LOGGED IN AS: " + github.accessToken);
+      }).catchError((e) {
+        print("LOGIN ERROR: " + e.toString());
       });
     }
   }
 
-  void _onClick() {
-    var url = Uri.parse(redirectUrl);
-    var oauthUrl = _grant.getAuthorizationUrl(url);
-    debugPrint("ouath-url - $oauthUrl");
-    launch(
-      oauthUrl.toString(),
-      forceSafariVC: false,
-      forceWebView: false,
-    );
+  void onClickGitHubLoginButton() async {
+    const String url = "https://github.com/login/oauth/authorize"
+        "?client_id=$clientId"
+        "&scope=public_repo%20read:user%20user:email";
+
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+      );
+    } else {
+      print("CANNOT LAUNCH THIS URL!");
+    }
   }
 
   @override
@@ -67,7 +130,7 @@ class _FooState extends State<Foo> {
       body: Container(
         child: RaisedButton(
           onPressed: () {
-            _onClick();
+            onClickGitHubLoginButton();
           },
           child: Text(
             "Login",
